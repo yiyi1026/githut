389a4abbf09d6c770b5dea694beaf61eab8c6527
yiyi1026 <vivian.li.uibe@gmail.com>
2017-10-31 14:08:45 -0700

    initial commit

---
+++ frontend/githut.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+// Testing Start
+// import * as APIUtil from './util/session_api_util';
+// import { login, logout, signup}  from './util/session_api_util';
+
+// import { login, logout, signup}  from './actions/session_actions';
+// import {} from './actions/question_actions';
+
+// Test Ends
+window.login = login;
+window.logout = logout;
+window.signup = signup;
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+  const preloadedState = { session: { currentUser: window.currentUser } };
+  store = configureStore(preloadedState);
+} else {
+  store = configureStore();
+}
+  // console.log(store);
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
+// ReactDOM.render(<div>entry file</div>, root);
