724ef283a292b6920750f26ee62913af55547398
yiyi1026 <vivian.li.uibe@gmail.com>
2017-10-31 20:49:21 -0700

    modify user model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,37 @@
 class Api::UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
+		@user = User.new(user_params)
 
-  end
-
-
-  def delete
-
-  end
-
+		if @user.save
+			login(@user)
+			render "api/users/show"  # user profile page
+		else
+			render json: @user.errors.full_messages, status: 422
+		end
+	end
   
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+  
+  def update
+    
+  end
+
+  def destroy
+
+  end
+  
+  private
+
+  def user_params
+		params.require(:user).permit(:username, :password, :email)
+	end
+  # username is not unique, email is!!!
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,13 @@
 #
 
 class User < ApplicationRecord
-  # do we need index username?? should it be unique???
+  # validations
 	validates :username, :password_digest, :session_token, presence: true
 	validates :email, uniqueness: true, presence: true
 	# validates :email, email_format: { message: "invalid email address format" }
 	validates :password, length: {minimum: 6}, allow_nil: :true
 
 	after_initialize :ensure_session_token
-  #do we need it?
 	before_validation :ensure_session_token_uniqueness
 
   attr_reader :password
@@ -31,19 +30,19 @@ class User < ApplicationRecord
 		self.password_digest = BCrypt::Password.create(password)
 	end
 
+	def is_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
+	end
+
 	def self.find_by_credentials(email, password)
 		user = User.find_by(email: email)
 		user && user.is_password?(password) ? user : nil
 	end
 
-	def is_password?(password)
-		BCrypt::Password.new(self.password_digest).is_password?(password)
-	end
-
 	def reset_session_token!
 		# self.session_token = SecureRandom.base64
 		ensure_session_token_uniqueness
-		self.save
+		self.save!
 		self.session_token
 	end
 
